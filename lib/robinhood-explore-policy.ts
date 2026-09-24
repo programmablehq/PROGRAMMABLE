e2646bc09a3c2e077bd64c94d46ec79f594de87a
Hazar <258789013+hazarxyz@users.noreply.github.com>
2026-09-25 00:23:42 +0200

    Hide TEST launch from Robinhood Explore

--- lib/robinhood-explore-policy.ts
+++ lib/robinhood-explore-policy.ts
@@ -6,6 +6,7 @@ export const PINNED_ROBINHOOD_TOKEN = "0xc60ba256b44334a0cd2c7242e98b88f031abb00
 const EXPLORE_EXCLUDED_TOKENS = new Set([
   "0xe8b292783382c93706dc43b54bba03f0f1a9908e", // catch trade
   "0x9fa5619b14d3fb6900247db219a3feb657ca46fc", // Tradable
+  "0x859e6b4497f977b8ad88fbde2c6ca70c68cec5f9", // TEST launch
 ]);
 
 export function isDiscoverableRobinhoodToken(address: string) {
